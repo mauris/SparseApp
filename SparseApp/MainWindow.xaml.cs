efceb2a8ac31b73778a3c1b93a81995a7043e3ee
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-02 10:02:29 +0800

    modified main window to metrowindow

--- SparseApp/MainWindow.xaml.cs
+++ SparseApp/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace SparseApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
