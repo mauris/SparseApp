0829302d3daaedc7833a913823e8298ff4d00bb0
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-02 18:51:17 +0800

    added mock repository manager

---
+++ SparseApp/Repository/MockManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SparseApp.Repository
+{
+    public class MockManager : Manager
+    {
+        public MockManager()
+            : base("")
+        {
+            repositories = new List<Repository>();
+            repositories.Add(new Repository { Path = @"C:\Users\Sam\Documents\GitHub\router" });
+            repositories.Add(new Repository { Path = @"C:\Users\Sam\Documents\GitHub\config" });
+            repositories.Add(new Repository { Path = @"C:\Users\Sam\Documents\GitHub\packfire-framework" });
+            repositories.Add(new Repository { Path = @"C:\Users\Sam\Documents\GitHub\cdac-rms" });
+        }
+    }
+}
