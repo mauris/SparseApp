1ec0ed9ad785030ba6bcd6075fd1ad4af59c757c
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-02 18:51:07 +0800

    added basename for repos

--- SparseApp/Repository/Repository.cs
+++ SparseApp/Repository/Repository.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using ProtoBuf;
 
 namespace SparseApp.Repository
 {
     [ProtoContract]
-    class Repository
+    public class Repository
     {
         [ProtoMember(1)]
         public string Path { get; set; }
+
+        public string Basename
+        {
+            get
+            {
+                FileInfo fi = new FileInfo(this.Path);
+                return fi.Name;
+            }
+        }
     }
 }
