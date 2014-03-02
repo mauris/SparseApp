765ea0a97ef01ed15eaae7f8f79db50e66e52002
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-02 20:03:30 +0800

    updated plugin manager

--- SparseApp/Plugin/Plugin.cs
+++ SparseApp/Plugin/Plugin.cs
@@ -2,12 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using ProtoBuf;
 
 namespace SparseApp.Plugin
 {
-    [ProtoContract]
     public class Plugin
     {
+
     }
 }
