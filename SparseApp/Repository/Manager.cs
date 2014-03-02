7b5ca6857c1010f9cc91757961f01e21f0fb68c4
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-02 15:48:34 +0800

    implemented basic repository manager

--- SparseApp/Repository/Manager.cs
+++ SparseApp/Repository/Manager.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using ProtoBuf;
 
 namespace SparseApp.Repository
 {
     class Manager
     {
+        protected string configFile;
+
+        protected List<Repository> repositories = new List<Repository>();
+
+        public Manager(string file)
+        {
+            this.configFile = file;
+        }
+
+        public void LoadRepositories()
+        {
+            using (var file = File.OpenRead(this.configFile))
+            {
+                repositories = Serializer.Deserialize<List<Repository>>(file);
+            }
+        }
+
+        public void SaveRepositories()
+        {
+            using (var file = File.Create(this.configFile))
+            {
+                Serializer.Serialize(file, repositories);
+            }
+        }
     }
 }
