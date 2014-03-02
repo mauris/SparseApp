d16cbaf8d6902007063eebb1c88b843954b797d3
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-02 15:30:46 +0800

    added repository object

---
+++ SparseApp/Repository/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProtoBuf;
+
+namespace SparseApp.Repository
+{
+    [ProtoContract]
+    class Repository
+    {
+        [ProtoMember(1)]
+        public string Path { get; set; };
+    }
+}
