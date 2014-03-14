9e15a6ff824fa4ca878fe9f5df5fbb94a2be73d9
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-14 22:29:04 +0800

    added plugins mock manager

---
+++ SparseApp/Plugin/MockManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SparseApp.Plugin
+{
+    public class MockManager : Manager
+    {
+        public MockManager(string folder)
+            : base("")
+        {
+        }
+
+        public override void LoadAvailablePlugins()
+        {
+        }
+    }
+}
