6d30545a4678e1d23fb0f0a9319a8f3eb0cd4fa9
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-15 07:26:35 +0800

    added two mock plugin sample

--- SparseApp/Plugin/MockManager.cs
+++ SparseApp/Plugin/MockManager.cs
@@ -10,6 +10,10 @@ namespace SparseApp.Plugin
         public MockManager(string folder)
             : base("")
         {
+            plugins.Add("Composer Install", new Plugin { Command = "composer install", Name = "Composer Install" });
+            plugins.Add("Composer Update", new Plugin { Command = "composer update", Name = "Composer Update" });
+            plugins.Add("git pull", new Plugin { Command = "git pull", Name = "git pull" });
+            plugins.Add("PHPUnit", new Plugin { Command = "phpunit -c test", Name = "PHPUnit" });
         }
 
         public override void LoadAvailablePlugins()
