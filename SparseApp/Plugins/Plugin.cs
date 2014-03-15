fce02f6f1172d0a25e7c71d00393fac6d287b02a
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-15 10:07:50 +0800

    updated plugin to execute

--- SparseApp/Plugins/Plugin.cs
+++ SparseApp/Plugins/Plugin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace SparseApp.Plugins
 {
@@ -14,5 +15,41 @@ namespace SparseApp.Plugins
         public string Name { get; set; }
 
         public string Command { get; set; }
+
+        protected Process process;
+
+        protected string output = "";
+
+        public string Output
+        {
+            get
+            {
+                if (process != null)
+                {
+                    output += process.StandardOutput.ReadToEnd();
+                }
+                return output;
+            }
+        }
+
+        public void Run(string path)
+        {
+            output = "";
+
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("cmd", "/c " + Command);
+
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.WorkingDirectory = path;
+            procStartInfo.CreateNoWindow = true;
+
+            process = new Process();
+            process.StartInfo = procStartInfo;
+            process.Start();
+
+            string result = process.StandardOutput.ReadToEnd();
+
+            Console.WriteLine(result);
+        }
     }
 }
