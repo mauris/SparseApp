ede2f32fec338cd0e680e502eb3b444547e31e06
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-22 17:29:48 +0800

    changing startup object

---
+++ SparseApp/Sparse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace SparseApp
+{
+    class Sparse
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            App app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
