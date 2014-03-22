f13141ec9d879d92e7734e157f28108944484129
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-22 17:37:27 +0800

    added clean up for app

--- SparseApp/Sparse.cs
+++ SparseApp/Sparse.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using Mono.Options;
+using System.IO.IsolatedStorage;
 
 namespace SparseApp
 {
@@ -11,9 +13,27 @@ namespace SparseApp
         [STAThread]
         public static void Main(string[] args)
         {
-            App app = new App();
-            app.InitializeComponent();
-            app.Run();
+            bool cleanup = false;
+            OptionSet set = new OptionSet()
+            {
+               {
+                   "cleanup", "Clean up the isolated storage used by Sparse when uninstlaling", v => cleanup = v != null
+               }
+            };
+
+            if (cleanup)
+            {
+                using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForAssembly())
+                {
+                    store.Remove();
+                }
+            }
+            else
+            {
+                App app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
         }
     }
 }
