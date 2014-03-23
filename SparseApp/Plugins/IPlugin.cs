311fbef61a3d20973a2568e9ab2080e6df0aca5e
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-23 09:42:53 +0800

    added plugin interface

---
+++ SparseApp/Plugins/IPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SparseApp.Plugins
+{
+    public interface IPlugin
+    {
+        string Name { get; set; }
+
+        void Run(string path);
+
+        string GetOutput();
+
+        bool IsRunning();
+
+        void Halt();
+    }
+}
