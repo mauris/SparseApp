62963b3d9cb85ef5b083a6ffb3bc9f9f5de2f0b6
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-23 09:20:40 +0800

    overriding constructor of app

--- SparseApp/App.xaml.cs
+++ SparseApp/App.xaml.cs
@@ -11,6 +11,11 @@ namespace SparseApp
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {
+    {        
+        public App()
+            : base()
+        {
+
+        }
     }
 }
