a3197692e3645b8d75dc01518b28a77cf9183582
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-23 09:28:23 +0800

    added default ninject module

---
+++ SparseApp/DefaultModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ninject.Modules;
+using SparseApp.Plugins;
+using SparseApp.Repositories;
+
+namespace SparseApp
+{
+    class DefaultModule: NinjectModule 
+    {
+        public override void Load()
+        {
+            Bind<IPluginManager>().To<PluginManager>();
+            Bind<IRepositoryManager>().To<RepositoryManager>();
+        }
+    }
+}
