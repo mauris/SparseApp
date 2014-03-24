d8567eb4b2cf90456e05590a2656311976a5b18b
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-24 20:55:58 +0800

    moved about flyout to its own control

---
+++ SparseApp/Views/AboutFlyout.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using System.Reflection;
+
+namespace SparseApp.Views
+{
+    /// <summary>
+    /// Interaction logic for AboutFlyout.xaml
+    /// </summary>
+    public partial class AboutFlyout : Flyout
+    {
+        public AboutFlyout()
+        {
+            InitializeComponent();
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            txtAppVersion.Text = "version " + assembly.GetName().Version.ToString();
+        }
+    }
+}
