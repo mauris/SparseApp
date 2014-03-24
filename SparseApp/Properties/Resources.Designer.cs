5d182c1264644d30a15de12f9e364e151e500504
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-24 22:21:31 +0800

    added license for sparse

--- SparseApp/Properties/Resources.Designer.cs
+++ SparseApp/Properties/Resources.Designer.cs
@@ -184,6 +184,24 @@ namespace SparseApp.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to The BSD 3-Clause License
+        ///
+        ///Copyright (c) 2014, Sam-Mauris Yong
+        ///All rights reserved.
+        ///
+        ///Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+        ///
+        ///1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+        ///
+        ///2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documenta [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string LicenseSparseApp {
+            get {
+                return ResourceManager.GetString("LicenseSparseApp", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The MIT License (MIT)
         ///
         ///Copyright (c) 2009 Osamu TAKEUCHI &lt;osamu@big.jp&gt;
