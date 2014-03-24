bf3a99a18e4bc5c428b0a05cb699719fcd72b62c
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-24 21:19:30 +0800

    added OpenSourceLicense model

---
+++ SparseApp/Models/OpenSourceLicense.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SparseApp.Models
+{
+    public class OpenSourceLicense
+    {
+        public string Name { get; set; }
+
+        public string Website { get; set; }
+
+        public string License { get; set; }
+    }
+}
