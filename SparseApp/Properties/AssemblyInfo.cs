4039c4611b066041f92f4c189c6fffb2d81f0eca
Sam-Mauris Yong <mauris@hotmail.sg>
2014-03-27 21:23:39 +0800

    updated assembly info

--- SparseApp/Properties/AssemblyInfo.cs
+++ SparseApp/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SparseApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Sparse")]
+[assembly: AssemblyDescription("Sparse Application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SparseApp")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("Sparse")]
+[assembly: AssemblyCopyright("Copyright © Sam-Mauris Yong 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
