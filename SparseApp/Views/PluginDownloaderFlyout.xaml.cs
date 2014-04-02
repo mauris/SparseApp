bb2428bc0a0d308a671a10b99480a1c5593ed44e
Sam-Mauris Yong <mauris@hotmail.sg>
2014-04-02 19:53:01 +0800

    added search code to download plugins meta from github #8

--- SparseApp/Views/PluginDownloaderFlyout.xaml.cs
+++ SparseApp/Views/PluginDownloaderFlyout.xaml.cs
@@ -8,10 +8,13 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
 
 namespace SparseApp.Views
 {
@@ -20,9 +23,65 @@ namespace SparseApp.Views
     /// </summary>
     public partial class PluginDownloaderFlyout : Flyout
     {
+        protected Thread searchAction;
+
         public PluginDownloaderFlyout()
         {
             InitializeComponent();
         }
+
+        private void txtSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (searchAction != null)
+            {
+                searchAction.Abort();
+                searchAction = null;
+            }
+            searchAction = new Thread(new ThreadStart(Search));
+            searchAction.IsBackground = true;
+            searchAction.Start();
+        }
+
+        private void Search()
+        {
+            try
+            {
+                Thread.Sleep(500);
+                string term = "";
+                Dispatcher.Invoke(new Action(() => term = txtSearch.Text));
+
+                WebClient client = new WebClient();
+                client.Headers.Add("Accept", "application/vnd.github.v3+json");
+                client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0;" +
+                   ".NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; InfoPath.2;" +
+                   ".NET CLR 3.5.21022; .NET CLR 3.5.30729; .NET4.0C; .NET4.0E)");
+                client.UseDefaultCredentials = true;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+                string data = client.DownloadString(new Uri("https://api.github.com/repos/mauris/SparseApp.Plugins/git/trees/master"));
+                JObject objects = JsonConvert.DeserializeObject<JObject>(data);
+                List<PluginFile> plugins = objects["tree"]
+                    .Where(item => ((string)item["path"]).EndsWith(".yml", StringComparison.InvariantCultureIgnoreCase))
+                    .Select(item => new PluginFile()
+                    {
+                        Name = Path.GetFileNameWithoutExtension(item["path"].ToString()),
+                        Url = item["url"].ToString(),
+                        Size = item["size"].ToString(),
+                        Hash = item["sha"].ToString()
+                    })
+                    .ToList<PluginFile>();
+            }
+            catch (ThreadAbortException)
+            {
+
+            }
+        }
+    }
+
+    class PluginFile
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Size { get; set; }
+        public string Hash { get; set; }
     }
 }
