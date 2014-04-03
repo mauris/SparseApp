1cb643c60acb2a4684c2e788b5c726054a762989
Sam-Mauris Yong <mauris@hotmail.sg>
2014-04-03 20:42:13 +0800

    added Github repo plugins handler for #8

---
+++ SparseApp/Plugins/Github.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SparseApp.Plugins
+{
+    class Github
+    {
+        protected List<PluginFile> files = new List<PluginFile>();
+
+        protected DateTime? lastSearch = null;
+
+        public List<PluginFile> Files {
+            get {
+                return files;
+            }
+        }
+
+        public List<PluginFile> Search(string term)
+        {
+            if (!lastSearch.HasValue || DateTime.Compare(lastSearch.Value, DateTime.Now.AddHours(72)) > 0)
+            {
+                DownloadList();
+            }
+
+            return files.Where(item => item.Name.ToLower().Contains(term.ToLower())).ToList();
+        }
+
+        protected void DownloadList()
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("Accept", "application/vnd.github.v3+json");
+            client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0;" +
+               ".NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; InfoPath.2;" +
+               ".NET CLR 3.5.21022; .NET CLR 3.5.30729; .NET4.0C; .NET4.0E)");
+            client.UseDefaultCredentials = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
+            string data = client.DownloadString(new Uri("https://api.github.com/repos/mauris/SparseApp.Plugins/git/trees/master"));
+            JObject objects = JsonConvert.DeserializeObject<JObject>(data);
+            files = objects["tree"]
+                .Where(item => ((string)item["path"]).EndsWith(".yml", StringComparison.InvariantCultureIgnoreCase))
+                .Select(item => new PluginFile()
+                {
+                    Name = Path.GetFileNameWithoutExtension(item["path"].ToString()),
+                    Url = item["url"].ToString(),
+                    Size = item["size"].ToString(),
+                    Hash = item["sha"].ToString()
+                })
+                .ToList<PluginFile>();
+        }
+    }
+
+    class PluginFile
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Size { get; set; }
+        public string Hash { get; set; }
+    }
+}
--- SparseApp/Views/PluginDownloaderFlyout.xaml.cs
+++ SparseApp/Views/PluginDownloaderFlyout.xaml.cs
@@ -11,10 +11,9 @@ using System.Windows.Media;
 using System.Windows.Navigation;
 using MahApps.Metro.Controls;
 using System.Threading;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.Net;
 using System.IO;
+using SparseApp.Plugins;
 
 namespace SparseApp.Views
 {
@@ -49,39 +48,11 @@ namespace SparseApp.Views
                 Thread.Sleep(500);
                 string term = "";
                 Dispatcher.Invoke(new Action(() => term = txtSearch.Text));
-
-                WebClient client = new WebClient();
-                client.Headers.Add("Accept", "application/vnd.github.v3+json");
-                client.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0;" +
-                   ".NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; InfoPath.2;" +
-                   ".NET CLR 3.5.21022; .NET CLR 3.5.30729; .NET4.0C; .NET4.0E)");
-                client.UseDefaultCredentials = true;
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3;
-                string data = client.DownloadString(new Uri("https://api.github.com/repos/mauris/SparseApp.Plugins/git/trees/master"));
-                JObject objects = JsonConvert.DeserializeObject<JObject>(data);
-                List<PluginFile> plugins = objects["tree"]
-                    .Where(item => ((string)item["path"]).EndsWith(".yml", StringComparison.InvariantCultureIgnoreCase))
-                    .Select(item => new PluginFile()
-                    {
-                        Name = Path.GetFileNameWithoutExtension(item["path"].ToString()),
-                        Url = item["url"].ToString(),
-                        Size = item["size"].ToString(),
-                        Hash = item["sha"].ToString()
-                    })
-                    .ToList<PluginFile>();
             }
             catch (ThreadAbortException)
             {
 
             }
         }
-    }
-
-    class PluginFile
-    {
-        public string Name { get; set; }
-        public string Url { get; set; }
-        public string Size { get; set; }
-        public string Hash { get; set; }
     }
 }
